78fbf1f15f8bf28c26347955c4cac43d8754e276
HAMMAD ALI <hammadali.6023@gmail.com>
2024-12-15 00:25:20 +0500

    update

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -12,11 +12,11 @@ type Category = {
         <img
           src={category.image}
           alt={category.title}
-          className="w-full h-96  object-cover"
+          className="w-full lg:h-96 h-96  object-cover"
         />
   
         {/* Category Info */}
-        <div className=" absolute inset-0 bg-transparent flex-row   lg:pt-64 hover:bg-gray-200 hover:opacity-50 ">
+        <div className=" absolute inset-0 bg-transparent flex-row  lg:pt-80 pt-80 hover:bg-gray-200 hover:opacity-50 ">
           <div className="text-white bg-black bg-opacity-50 p-2 rounded-lg ">
             <h3 className="text-lg font-semibold">{category.title}</h3>
             <p className="text-sm">{category.productCount} Products</p>
