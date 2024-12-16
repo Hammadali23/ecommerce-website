b856a726c7a0d7158e2d590cda6d22e2b470be7b
HAMMAD ALI <hammadali.6023@gmail.com>
2024-12-16 10:42:06 +0500

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -111,7 +111,7 @@ const Header = () => {
             About
           </Link>
           <Link href="/contactUs" onClick={closeMenu} className="block text-gray-700 hover:text-teal-600">
-            ContacttUs
+            Contact Us
           </Link>
           <button className="flex items-center bg-white border border-gray-300 px-3 py-1 rounded-md w-full text-black">
             🛒 Cart
